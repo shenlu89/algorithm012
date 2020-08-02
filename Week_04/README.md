b53ea99bd916e4e3e9caa9f4636f6fe620137cf4
shenlu89 <shenlu89dev@gmail.com>
2020-08-02 11:09:16 +0800

    submit week 04 homework

--- Week_04/README.md
+++ Week_04/README.md
@@ -7,8 +7,8 @@
 - 柠檬水找零（亚马逊在半年内面试中考过）[860_LemonadeChange.md](https://github.com/shenlu89/algorithm012/blob/master/Week_04/860_LemonadeChange.md)
 - 买卖股票的最佳时机 II （亚马逊、字节跳动、微软在半年内面试中考过）[122_BestTimetoBuyandSellStockII.md](https://github.com/shenlu89/algorithm012/blob/master/Week_04/122_BestTimetoBuyandSellStockII.md)
 - 分发饼干（亚马逊在半年内面试中考过）
-- 模拟行走机器人
-- 使用二分查找，寻找一个半有序数组 [4, 5, 6, 7, 0, 1, 2] 中间无序的地方
+- 模拟行走机器人 
+- 使用二分查找，寻找一个半有序数组 [4, 5, 6, 7, 0, 1, 2] 中间无序的地方 [寻找一个半有序数组中间无序的地方.md](https://github.com/shenlu89/algorithm012/blob/master/Week_04/寻找一个半有序数组中间无序的地方.md)
 
 >说明：同学们可以将自己的思路、代码写在第 4 周的学习总结中
 
@@ -30,7 +30,7 @@
 ### 实战题目
 
 - 二叉树的层序遍历（字节跳动、亚马逊、微软在半年内面试中考过）[102_BinaryTreeLevelOrderTraversal.md](https://github.com/shenlu89/algorithm012/blob/master/Week_04/102_BinaryTreeLevelOrderTraversal.md)
-- 最小基因变化
+- 最小基因变化 
 - 括号生成（字节跳动、亚马逊、Facebook 在半年内面试中考过）[22_GenerateParentheses.md](https://github.com/shenlu89/algorithm012/blob/master/Week_03/22_GenerateParentheses.md)
 - 在每个树行中找最大值（微软、亚马逊、Facebook 在半年内面试中考过）[515_FindLargestValueinEachTreeRow.md](https://github.com/shenlu89/algorithm012/blob/master/Week_04/515_FindLargestValueinEachTreeRow.md)
 
--- Week_04/prices.js
+++ Week_04/prices.js
@@ -1,21 +1,46 @@
-var maxProfit = function(prices) {
-    if (!prices) return 0;
-    let sum = 0;
-    for (let i = 0, j = i+1, len = prices.length; j < len;) {
-        if (prices[i] < prices[j]) {
-            while (j < len && prices[j] <= prices[j+1]) j++;
-            sum += prices[j] - prices[i];
-            i = j;
-            j = i+1;
+// var maxProfit = function(prices) {
+//     if (!prices) return 0;
+//     let sum = 0;
+//     for (let i = 0, j = i+1, len = prices.length; j < len;) {
+//         if (prices[i] < prices[j]) {
+//             while (j < len && prices[j] <= prices[j+1]) j++;
+//             sum += prices[j] - prices[i];
+//             i = j;
+//             j = i+1;
+//         } else {
+//             i = j;
+//             j++;
+//         }
+//     }
+//     return sum;
+// };
+
+// let prices = [7,1,5,3,6,4];
+// // let prices = [1,2,3,4,5];
+
+// console.log(maxProfit(prices));
+
+var search = function (nums) {
+    let left = 0, right = nums.length-1;
+    if (nums[left] < nums[right]) return -1;
+    while (left < right) {
+        let mid = parseInt((left+right)/2);
+        if (nums[mid] > nums[mid + 1]) {
+            return mid + 1;
         } else {
-            i = j;
-            j++;
+            if (nums[mid] < nums[left]) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
         }
     }
-    return sum;
-};
+}
 
-let prices = [7,1,5,3,6,4];
-// let prices = [1,2,3,4,5];
+let nums = [4, 5, 6, 7, 0, 1, 2]; // 4
+// let nums = [1, 2, 3, 4] // -1
+// let nums = [6, 5] // 1
+// let nums = [2, 3, 4, 5, 1] // 4
+// let nums = [5, 1, 2, 3, 4] // 1
 
-console.log(maxProfit(prices));
+console.log(search(nums));
